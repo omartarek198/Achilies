c0c18aa1ef80c989965874e25adebc1a9c3c07c1
omartarek198 <omar.tarek29@msa.edu.eg>
2023-02-10 04:42:16 +0200

    seperated server functions in a new file

--- main.py
+++ main.py
@@ -1,55 +1,18 @@
 from flask import Flask
 from flask import request
 from flask import Response
-from automated_gui import *
-import requests
 import os
+from automated_gui import *
+from server_utils import *
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
 password = os.getenv('password')
 
 
 app = Flask(__name__)
-def parse_message(message):
-    print("message-->",message)
-    
-    chat_id = message['message']['chat']['id']
-    
-    txt = message['message']['text']
-    
-    print("chat_id-->", chat_id)
-    
-    print("txt-->", txt)
-    
-    return chat_id,txt
- 
-def tel_send_message(chat_id, text):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    payload = {
-                'chat_id': chat_id,
-                'text': text
-                }
-   
-    r = requests.post(url,json=payload)
-    return r
-def tel_send_image(chat_id,path):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id='
-    files = {'photo': open(path,'rb')}
-    payload = {
-        'chat_id': chat_id,
-        'image': files,
-        'caption': "This is a sample image"
-    }
-    
-    r = requests.post(url+ str(payload['chat_id']), files=files)
-    return r
- 
- 
-    
-    
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global validated
---
+++ server_utils.py
@@ -0,0 +1,44 @@
+
+from flask import Flask
+from flask import request
+from flask import Response
+from automated_gui import *
+import requests
+import os
+from dotenv import load_dotenv
+
+TOKEN = os.getenv('TOKEN')
+
+def parse_message(message):
+    print("message-->",message)
+    
+    chat_id = message['message']['chat']['id']
+    
+    txt = message['message']['text']
+    
+    print("chat_id-->", chat_id)
+    
+    print("txt-->", txt)
+    
+    return chat_id,txt
+
+
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+                'chat_id': chat_id,
+                'text': text
+                }
+   
+    r = requests.post(url,json=payload)
+    return r
+def tel_send_image(chat_id,path):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id='
+    files = {'photo': open(path,'rb')}
+    payload = {
+        'chat_id': chat_id,
+        'image': files,
+        'caption': "This is a sample image"
+    } 
+    r = requests.post(url+ str(payload['chat_id']), files=files)
+    return r 
